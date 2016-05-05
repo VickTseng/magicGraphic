57b6e71332ab4ab63422bb58fee0598c7043ed08
VickT <s8413896@gmail.com>
2016-05-05 15:18:12 +0800

    The first commit of OpenGL

---
+++ app/src/main/java/com/example/quanta/openglpactice/GLRender.java
@@ -0,0 +1,26 @@
+package com.example.quanta.openglpactice;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by 10210074 on 2016/5/5.
+ */
+public class GLRender implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        gl.glViewport(0,0,width,height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+    }
+}
---
+++ app/src/main/java/com/example/quanta/openglpactice/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.quanta.openglpactice;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    private GLSurfaceView glSurface;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glSurface = new GLSurfaceView(this);
+        glSurface.setRenderer(new GLRender());
+
+        setContentView(glSurface);
+
+
+
+
+    }
+
+}
